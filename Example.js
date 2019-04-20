fff483f6bb1411d3262bb8ef4fba1b781e1cdd5c
Josh Clark <jdougclark@gmail.com>
2019-04-20 13:50:29 -0700

    background color on mouseover hamburger

--- Example.js
+++ Example.js
@@ -1,8 +1,19 @@
 $(document).ready(() => {
   $('.hover').on('mouseover', () => {
       $('#box-animate').fadeIn();
-  })
+  });
   $('.hover-box').on('mouseleave', () => {
       $('#box-animate').fadeOut();
-  })
-})
+  });
+
+  $('.hamburgerIcon').on('mouseover', () => {
+      $('.hamburgerIcon').toggleClass('hamburgerHover');
+      $('.hamburgerIcon').on('mouseleave', () => {
+          $('.hamburgerIcon').toggleClass('hamburgerHover');
+        });
+  });
+
+
+
+
+});
