c7dc2c9ec3db14e5a7fceca6b637a1d726ec8ed9
Josh <jdougclark@gmail.com>
2019-04-22 11:15:30 -0700

    Animation for Dropdown

--- Example.js
+++ Example.js
@@ -11,9 +11,30 @@ $(document).ready(() => {
       $('.hamburgerIcon').on('mouseleave', () => {
           $('.hamburgerIcon').toggleClass('hamburgerHover');
         });
+      $('.hamburgerIcon').on('click', () => {
+        $('.hamburgerDropdown').fadeIn();
+      });
+      $('.hamburgerDropdown').on('mouseleave', () => {
+          $('.hamburgerDropdown').fadeOut();
+      });
+      $('.1').on('mouseover', () => {
+          $('.1').toggleClass('hamburgerHover');
+      });
+      $('.1').on('mouseleave', () => {
+          $('.1').toggleClass('hamburgerHover');
+      });
+      $('.2').on('mouseover', () => {
+          $('.2').toggleClass('hamburgerHover');
+      });
+      $('.2').on('mouseleave', () => {
+          $('.2').toggleClass('hamburgerHover');
+      });
+      $('.1').on('mouseover', () => {
+          $('.1').toggleClass('hamburgerHover');
+      });
+      $('.3').on('mouseleave', () => {
+          $('.3').toggleClass('hamburgerHover');
+      });
   });
-
-
-
 
 });
